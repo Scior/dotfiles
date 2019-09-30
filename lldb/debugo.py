20077af926478862bc72ae2b8174092cf31e59d6
Scior <interval12@gmail.com>
2019-10-01 00:12:52 +0900

    Add debugo.py

---
+++ lldb/debugo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import lldb
+
+def debugo(debugger, name, result, dict):
+    lldb.debugger.HandleCommand('e %s.debug("::%s").subscribe()' % (name, name))
+
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand('command script add -f debugo.debugo deo')
+
