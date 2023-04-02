d95ed7017a223c4f774757431ceeabab4a3da4cc
Scior <interval12@gmail.com>
2023-04-02 14:06:30 +0900

    Set VS Code as a merge tool

--- setenv.sh
+++ setenv.sh
@@ -10,7 +10,7 @@ ln -sf dotfiles/.vim .vim
 
 # HomeBrew
 cd $(dirname $0)
-which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew >/dev/null 2>&1 && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 which brew >/dev/null 2>&1 && brew doctor --verbose
 which brew >/dev/null 2>&1 && brew update --verbose
 which brew >/dev/null 2>&1 && brew bundle --file Brewfile --verbose
@@ -21,6 +21,12 @@ sh ./install.sh
 rm ./install.sh
 mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# VS Code
+git config --global merge.tool vscode
+git config --global mergetool.vscode.cmd 'code --wait --merge "$LOCAL" "$REMOTE" "$BASE" "$MERGED"'
+git config --global mergetool.prompt false
+git config --global mergetool.keepBackup false
 
 sudo chsh -s `which zsh` $USER
 
