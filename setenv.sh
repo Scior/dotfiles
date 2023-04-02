35aac5ddc977a40a7eb1a487b39040895905c063
Scior <interval12@gmail.com>
2023-04-02 13:30:31 +0900

    Add a Brewfile

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 cd ~
 ln -sf dotfiles/.vimrc .vimrc
 ln -sf dotfiles/.aliasrc .aliasrc
@@ -7,6 +8,14 @@ ln -sf dotfiles/.zshrc .zshrc
 ln -sf dotfiles/.bashrc .bashrc
 ln -sf dotfiles/.vim .vim
 
+# HomeBrew
+cd $(dirname $0)
+which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew >/dev/null 2>&1 && brew doctor --verbose
+which brew >/dev/null 2>&1 && brew update --verbose
+which brew >/dev/null 2>&1 && brew bundle --file Brewfile --verbose
+
+# Vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
 rm ./install.sh
