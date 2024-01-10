98a2c4663128eb8ddc2077bfe383b8392cde6d76
Suita Fujino <interval12@gmail.com>
2024-01-10 17:40:56 +0900

    Remove vim setup scripts

--- setenv.sh
+++ setenv.sh
@@ -18,15 +18,8 @@ else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew doctor --verbose
   brew update --verbose
-  brew bundle --file Brewfile --verbose
+  brew bundle --file dotfiles/Brewfile --verbose
 fi
-
-# Vim
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-sh ./install.sh
-rm ./install.sh
-mkdir ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 # VS Code
 git config --global merge.tool vscode
@@ -34,4 +27,4 @@ git config --global mergetool.vscode.cmd 'code --wait --merge "$LOCAL" "$REMOTE"
 git config --global mergetool.prompt false
 git config --global mergetool.keepBackup false
 
-sudo chsh -s `which zsh` $USER
+# sudo chsh -s `which zsh` $USER
